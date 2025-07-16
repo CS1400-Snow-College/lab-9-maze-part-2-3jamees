0e4161863b0bafd7fa2fc721c462b610e42aacfe
3jamees <jamesiiking8@gmail.com>
2025-07-16 19:15:40 -0400

    Added header and welcome

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Author: James King II
+// Date: July 15, 2025
+// Lab: Lab 9 - Maze #2
+
+    {
+        Console.WriteLine("Welcome to Maze #2!");
+        Console.WriteLine("Navigate through the maze, avoid the bad guys, and collect all coins.");
+        Console.WriteLine("Press any key to begin...");
+        Console.ReadKey();
+    }
+
