3bdee04145ec7672ae1d55c6db90168970491465
3jamees <jamesiiking8@gmail.com>
2025-07-16 19:45:42 -0400

    coin collection and score display

--- Program.cs
+++ Program.cs
@@ -84,3 +84,55 @@ do
 
 } while (key != ConsoleKey.Escape);
 
+int badGuy1Top = 5, badGuy1Left = 5;
+int badGuy2Top = 10, badGuy2Left = 10;
+Random rand = new Random();
+
+
+Console.SetCursorPosition(badGuy1Left, badGuy1Top);
+Console.Write(' ');
+Console.SetCursorPosition(badGuy2Left, badGuy2Top);
+Console.Write(' ');
+
+
+int move1 = rand.Next(4);
+if (move1 == 0) badGuy1Top++;
+else if (move1 == 1) badGuy1Top--;
+else if (move1 == 2) badGuy1Left++;
+else if (move1 == 3) badGuy1Left--;
+
+int move2 = rand.Next(4);
+if (move2 == 0) badGuy2Top++;
+else if (move2 == 1) badGuy2Top--;
+else if (move2 == 2) badGuy2Left++;
+else if (move2 == 3) badGuy2Left--;
+
+
+Console.SetCursorPosition(badGuy1Left, badGuy1Top);
+Console.Write('%');
+Console.SetCursorPosition(badGuy2Left, badGuy2Top);
+Console.Write('%');
+
+
+if ((top == badGuy1Top && left == badGuy1Left) ||
+    (top == badGuy2Top && left == badGuy2Left))
+{
+    Console.Clear();
+    Console.WriteLine("You Lose!");
+    return;
+}
+
+int score = 0;
+
+if (mapRows[top][left] == '^')
+{
+    mapRows[top] = mapRows[top].Remove(left, 1).Insert(left, " ");
+    Console.SetCursorPosition(left, top); Console.Write(' ');
+    score += 100;
+}
+
+
+Console.SetCursorPosition(0, mapRows.Length);
+Console.Write("Score: " + score);
+
+
